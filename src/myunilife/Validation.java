8f4c290cf281c230090ca4d69138de3d11cba85f
Pathmila <asinipathmila@gmail.com>
2021-04-29 16:44:12 +0530

    commit 1 (Login & Signup)

---
+++ src/myunilife/Validation.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myunilife;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Validation {
+    public static boolean validPassword(char[] pass, char[] repass) {
+        if(pass.length == repass.length){
+            for(int i=0; i<pass.length; i++){
+                if(pass[i]!= repass[i]){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
